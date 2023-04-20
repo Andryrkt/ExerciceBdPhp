1d3a8a3bfab441371f71737439b6b6530a72f59e
Hasina LTN ANDRIANADISON <andryrkt08@gmail.com>
2023-04-21 00:50:05 +0300

    0.0.3 ajout de boostra avec composer

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+require_once 'vendor/autoload.php';
 require_once 'conexion_Bd.php';
-$requete = $conexion->prepare("
-        INSERT INTO nomTable(nom, prenom, ");
+use twbs\bootstrap;
+// $requete = $conexion->prepare("
+//         INSERT INTO nomTable(nom, prenom, ");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +14,22 @@ $requete = $conexion->prepare("
     <title>Document</title>
 </head>
 <body>
-    
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        <span class="fs-4">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
+
 </body>
 </html>
